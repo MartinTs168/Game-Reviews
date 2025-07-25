eeec1af25744a5f48841090455b2305a9e29a9c5
MartinTs168 <martin.tsenev@gmail.com>
2025-07-25 21:20:58 +0300

    created a mixin to check if the user trying to access the resource is the same as the owner

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import get_user_model, login
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView
 
 from accounts.forms import AppUserCreationForm, ProfileEditForm
 from accounts.models import Profile
+from common.mixins import UserIsOwnerMixin
 
 # Create your views here.
 UserModel = get_user_model()
@@ -26,14 +27,14 @@ class UserRegisterView(CreateView):
         return response
 
 
-class ProfileDetailsView(DetailView):
+class ProfileDetailsView(LoginRequiredMixin, UserIsOwnerMixin, DetailView):
     model = Profile
     context_object_name = 'profile'
     template_name = 'accounts/profile-details.html'
     queryset = Profile.objects.prefetch_related('user')
 
 
-class ProfileEditView(UpdateView):
+class ProfileEditView(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
     model = Profile
     form_class = ProfileEditForm
     template_name = 'accounts/profile-edit.html'
