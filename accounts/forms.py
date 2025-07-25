8c1f965b466864c00e54285741c33a386d9a8296
MartinTs168 <martin.tsenev@gmail.com>
2025-07-25 17:56:58 +0300

    created form for profile edit and removed unnecessary links from base page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,23 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+
+from accounts.models import Profile
 
 UserModel = get_user_model()
+
 
 class AppUserCreationForm(UserCreationForm):
     class Meta:
         model = UserModel
         fields = ('username', 'email')
+
+
+class ProfileBaseForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class ProfileEditForm(ProfileBaseForm):
+    class Meta(ProfileBaseForm.Meta):
+        exclude = ('user', )
