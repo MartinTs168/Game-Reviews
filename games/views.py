fbd31e09c2dd6a5477addad4f1ad21ed41400de4
MartinTs168 <martin.tsenev@gmail.com>
2025-07-31 20:54:47 +0300

    changed success url of create game and edit game to details page

--- games/views.py
+++ games/views.py
@@ -16,16 +16,20 @@ class GameCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Game
     form_class = GameCreateForm
     template_name = 'games/create-game.html'
-    success_url = reverse_lazy('all-games')
     permission_required = 'games.add_game'
+
+    def get_success_url(self):
+        return reverse_lazy('game-details', kwargs={'pk': self.object.pk})
 
 
 class GameEditView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Game
     form_class = GameEditForm
     template_name = 'games/edit-game.html'
-    success_url = reverse_lazy('all-games')
     permission_required = 'games.change_game'
+
+    def get_success_url(self):
+        return reverse_lazy('game-details', kwargs={'pk': self.object.pk})
 
 
 class GameDetailsView(DetailView):
