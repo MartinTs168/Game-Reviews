7089e95af86b59bb2b0df6b632a18f7bea4c1a02
MartinTs168 <martin.tsenev@gmail.com>
2025-08-01 22:13:05 +0300

    fix in the user_in_group_filter now returns true if user is superuser and a small change to game details page

--- common/templatetags/user_in_group_filter.py
+++ common/templatetags/user_in_group_filter.py
@@ -1,8 +1,10 @@
 from django import template
 
+from accounts.models import AppUser
+
 register = template.Library()
 
 
 @register.filter(name='has_group')
-def has_group(user, group_name):
-    return user.groups.filter(name=group_name).exists()
+def has_group(user: AppUser, group_name: str) -> bool:
+    return user.is_superuser or user.groups.filter(name=group_name).exists()
