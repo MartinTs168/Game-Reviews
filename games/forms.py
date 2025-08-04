432478c693cd3fff7d92b604e1840285d2fae919
MartinTs168 <martin.tsenev@gmail.com>
2025-08-05 01:12:47 +0300

    updated game edit and game create pages

--- games/forms.py
+++ games/forms.py
@@ -1,5 +1,5 @@
 from django.forms.models import ModelForm
-from django.forms.widgets import CheckboxSelectMultiple
+from django.forms.widgets import CheckboxSelectMultiple, TextInput, DateInput, Textarea, FileInput
 
 from games.models import Game
 
@@ -10,13 +10,25 @@ class GameBaseForm(ModelForm):
         fields = '__all__'
 
         widgets = {
-            'tags': CheckboxSelectMultiple()
+            'tags': CheckboxSelectMultiple(),
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'developer': TextInput(attrs={'class': 'form-control'}),
+            'publisher': TextInput(attrs={'class': 'form-control'}),
+            'available_platforms': TextInput(attrs={'class': 'form-control'}),
+            'release_date': DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date'
+            }),
+            'description': Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5
+            }),
+            'image': FileInput(attrs={'class': 'form-control'}),
         }
 
 
 class GameCreateForm(GameBaseForm):
-    class Meta(GameBaseForm.Meta):
-        exclude = ('tags',)
+    ...
 
 
 class GameEditForm(GameBaseForm):
