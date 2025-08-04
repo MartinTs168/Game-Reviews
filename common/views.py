a4bbcbfc5e158703de4a149f274f898c5f27ec83
MartinTs168 <martin.tsenev@gmail.com>
2025-08-04 21:43:24 +0300

    redesign home page

--- common/views.py
+++ common/views.py
@@ -1,7 +1,17 @@
+from django.db.models.aggregates import Avg, Count
 from django.http import HttpRequest
 from django.shortcuts import render
+
+from games.models import Game
 
 
 # Create your views here.
 def home(request: HttpRequest):
-    return render(request, 'common/home-page.html')
+    top_game = Game.objects.annotate(
+        avg_rating=Avg('ratings__value'),
+        ratings_count=Count('ratings')
+    ).filter(ratings_count__gt=0).order_by('-avg_rating', '-ratings_count').first()
+    context = {
+        'top_game': top_game,
+    }
+    return render(request, 'common/home-page.html', context=context)
