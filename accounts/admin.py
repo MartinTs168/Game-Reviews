84bedbd246927cfc38c7424ac0de93e639a5286f
MartinTs168 <martin.tsenev@gmail.com>
2025-08-04 14:27:21 +0300

    added AppUser to the django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import AppUser
+
+
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    filter_horizontal = ('groups', 'user_permissions')
