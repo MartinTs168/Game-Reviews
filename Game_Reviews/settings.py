1296537e79172a54aa46531852d7c715f536f860
MartinTs168 <martin.tsenev@gmail.com>
2026-09-19 16:11:40 +0300

    removed workflow for azure deployment and changed the connection to use a connection string rather than all parameteres by themselves

--- Game_Reviews/settings.py
+++ Game_Reviews/settings.py
@@ -96,19 +96,10 @@ WSGI_APPLICATION = 'Game_Reviews.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
-        'OPTIONS': {
-            'sslmode': 'require'
-        },
-    }
+    'default': env.db('DB_URL')
 }
 
+DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
 if DEBUG:
     DATABASES['default']['OPTIONS']['sslmode'] = 'prefer'
 
